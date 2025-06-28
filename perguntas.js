d4c32d43c3bacf0d6cc5b55fd682f878bbb047f8
FabioSenna <fabio.marostica@escola.pr.gov.br>
2025-06-28 01:27:20 +0000

    .

---
+++ perguntas.js
@@ -0,0 +1,31 @@
+criaCartao (
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao (
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao (
+    'Geografia',
+    'Qual é o rio mais extenso do mundo?',
+    'Rio Nilo'
+
+)
+
+criaCartao (
+    'Matematica',
+    'Qual é a raiz de 144?',
+    '12'
+)
+
+criaCartao (
+    'Biologia',
+    'Qual parte do corpo humano produz insulina?',
+    'Pâncreas'
+)
+
