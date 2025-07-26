947688e5fbda103fac4e96971eca6f19ff1f3de8
FabioSenna <fabio.marostica@escola.pr.gov.br>
2025-07-26 23:59:58 +0000

    .

--- perguntas.js
+++ perguntas.js
@@ -5,12 +5,6 @@ criaCartao (
 )
 
 criaCartao (
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
-)
-
-criaCartao (
     'Geografia',
     'Qual é o rio mais extenso do mundo?',
     'Rio Nilo'
@@ -29,3 +23,68 @@ criaCartao (
     'Pâncreas'
 )
 
+criaCartao(
+  'Química',
+  'Qual o símbolo químico da água?',
+  'H2O'
+)
+
+criaCartao(
+  'Resolução de Problemas',
+  'Se um trem sai às 8h e outro às 9h, qual chegará primeiro se ambos têm a mesma velocidade?',
+  'O trem que saiu às 8h'
+)
+
+criaCartao(
+  'Biotecnologia',
+  'O que é transgênico?',
+  'Organismo que recebeu genes de outra espécie'
+)
+
+criaCartao(
+  'História',
+  'Em que ano ocorreu a Proclamação da República no Brasil?',
+  '1889'
+)
+
+criaCartao(
+  'Português',
+  'Qual é o sujeito na frase: "Os alunos estudam muito"?',
+  'Os alunos'
+)
+
+criaCartao(
+  'Sociologia',
+  'O que é cultura?',
+  'Conjunto de hábitos, costumes e valores de um povo'
+)
+
+criaCartao(
+  'Matemática II',
+  'Qual é o valor de π (pi) aproximado?',
+  '3,14'
+)
+
+criaCartao(
+  'Projeto de Vida',
+  'O que significa ter um projeto de vida?',
+  'Definir metas e caminhos para o futuro'
+)
+
+criaCartao(
+  'Inglês',
+  'Traduza: "Good morning".',
+  'Bom dia'
+)
+
+criaCartao(
+  'Educação Financeira',
+  'O que é orçamento?',
+  'Planejamento das receitas e despesas'
+)
+
+criaCartao(
+  'Física I',
+  'Qual unidade é usada para medir força?',
+  'Newton (N)'
+)
